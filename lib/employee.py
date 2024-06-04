9c572d78799cb8864c6638b5f7bd88d12fefedf6
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-06-05 02:04:35 +0300

    ORM Lab

--- lib/employee.py
+++ lib/employee.py
@@ -1,4 +1,3 @@
-# lib/employee.py
 from __init__ import CURSOR, CONN
 from department import Department
 
@@ -187,4 +186,12 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review  # import Review class here to avoid circular import issues
+        sql = """
+            SELECT *
+            FROM reviews
+            WHERE employee_id = ?
+        """
+        rows = CURSOR.execute(sql, (self.id,)).fetchall()
+        return [Review.instance_from_db(row) for row in rows]
+
